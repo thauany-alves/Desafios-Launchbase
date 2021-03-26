fac631ef5333852ee32bfc6c732bd36ce4926a5c
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-03-26 00:08:07 -0300

    Create members and show member

--- Modulo-04/controllers/members.js
+++ Modulo-04/controllers/members.js
@@ -40,27 +40,27 @@ exports.post = function (req, res) {
     }
   }
 
-  let {avatar_url,name, birth, services,gender,} = req.body;
-
-  birth = Date.parse(birth);
+  birth = Date.parse(req.body.birth);
   const created_at = Date.now();
-  const id = Number(data.members.length + 1);
-  
+
+  let id = 1;
+  const lastMember = data.members[data.members.length - 1];
+ 
+  if(lastMember){
+    id = lastMember.id + 1
+  }
   
   data.members.push({
     id,
-    name, 
-    avatar_url,
+    ...req.body,
     birth, 
-    gender,
     created_at
   });
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
     if(err) return res.send("write file err!");
-    return res.redirect("/members");
+    return res.redirect(`/members/${id}`);
   })
-  // return res.send(req.body);
 }
 
 //update
